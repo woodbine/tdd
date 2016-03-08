2d4183648f40572fe16a0337ba7f578046d39227
Ian Makgill <ian@spendnetwork.com>
2016-03-08 09:27:31 +0000

    Basic view now returns minimal HTML

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -7,8 +7,8 @@ class NewVisitorText(unittest.TestCase):
 		self.browser = webdriver.Firefox()
 		self.browser.implicitly_wait(3)
 
-	def tearDown(self):
-		self.browser.quit()
+	# def tearDown(self):
+		# self.browser.quit()
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
 
